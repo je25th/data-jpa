efb8b5268eadb4265be82ec277011287521d8342
je25th <je25th@kakao.com>
2022-06-13 22:39:58 +0900

    도메인 모델 작성중

--- src/main/kotlin/study/datajpa/entity/Member.kt
+++ src/main/kotlin/study/datajpa/entity/Member.kt
@@ -1,12 +1,32 @@
 package study.datajpa.entity
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 @Entity
 class Member(
     @Id @GeneratedValue
+    @Column(name="member_id")
     val id: Long? = null,
-    var username: String
-)
+    var username: String?,
+    var age: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "team_id") // fk
+    var team: Team? = null
+) {
+
+    fun changeTema(team: Team) {
+        this.team = team
+        team.member.add(this)
+    }
+
+    override fun toString(): String {
+        //무한루프에 빠질 위험성이 있기 때문에 연관관계 필드는 toString에 안넣는 것을 권장
+        return "Member(id=$id, username='$username', age=$age)"
+    }
+}
---
+++ src/main/kotlin/study/datajpa/entity/Team.kt
@@ -0,0 +1,22 @@
+package study.datajpa.entity
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.OneToMany
+
+@Entity
+class Team(
+    @Id @GeneratedValue
+    @Column(name = "tema_id")
+    val id: Long? = null,
+    var name: String,
+
+    @OneToMany(mappedBy = "team")
+    var member: MutableList<Member>? = mutableListOf()
+) {
+    override fun toString(): String {
+        return "Team(id=$id, name='$name')"
+    }
+}
